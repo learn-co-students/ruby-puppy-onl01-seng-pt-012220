e21e0b5844a497c2c24fac350cb195acea6d1efb
Xavier Perez <xperez007@gmail.com>
2020-02-10 22:32:48 +0000

    Done.

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,1 +1,32 @@
-# Add your code here
+class Dog  
+  attr_accessor :name
+  attr_reader :breed 
+  @@all = []
+  
+def self.all
+  @@all 
+  
+end
+
+  
+  
+  
+def initialize(name) 
+  @name = name
+  @breed = breed
+  @@all << self
+end
+
+def self.clear_all
+  @@all = []
+
+end
+
+end
+
+
+
+
+
+
+
